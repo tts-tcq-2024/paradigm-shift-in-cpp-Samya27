862f62bffd6c26dab9cb51e6f465067e71042476
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 14:58:16 +0530

    Update PrintMessage.cpp

--- PrintMessage.cpp
+++ PrintMessage.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include <string>
+#include <map>
 #include "PrintMessage.h"
 
-Language currentLanguage = ENGLISH;
-void printMessage(const std::string& messageEnglish, const std::string& messageGerman)
+Language PrintLanguage = ENGLISH; 
+map<string, string> ErrorMessageEnglish = 
+                    {{"Temperature", "Temperature out of range!"}, 
+                    {"soc", "State of Charge out of range!"},
+                    {"chargeRate", "Charge Rate out of range!"},
+                    {"BatteryOK", "Battery status is OK!"},
+                    {"BatteryNotOK", "Battery status is not OK!"}};
+map<string, string> ErrorMessageGerman = 
+                    {{"Temperature", "Temperatur außerhalb des Bereichs!"}, 
+                    {"soc", "Ladezustand außerhalb des Bereichs!"},
+                    {"chargeRate", "Lade-Rate außerhalb des Bereichs!"},
+                    {"BatteryOK", "Batteriezustand ist ok!"},
+                    {"BatteryNotOK", "Batteriezustand ist nicht ok!"}};
+
+void printMessage(const Language PrintLanguage, const std::string& key)
 {
-    if (currentLanguage == ENGLISH) {
+    if (PrintLanguage == ENGLISH) {
+    std:string messageEnglish= ErrorMessageEnglish.find(key);    
     std::cout << messageEnglish << "\n";
-    } else if (currentLanguage == GERMAN) {
+    } else if (PrintLanguage == GERMAN) {
+        std:string messageGerman= ErrorMessageGerman.find(key);
         std::cout << messageGerman << "\n";
     }
 }
