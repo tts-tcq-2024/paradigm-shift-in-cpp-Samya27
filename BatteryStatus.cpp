e3610fb23856fd62609bfe9cb5585f5695f1d536
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 15:51:34 +0530

    Update BatteryStatus.cpp

--- BatteryStatus.cpp
+++ BatteryStatus.cpp
@@ -66,13 +66,13 @@ bool batteryIsOk(float temperature, float soc, float chargeRate)
   }
   return(BatteryStatus);
 }
-int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
-  assert(batteryIsOk(55, 65, 1) == false);
-  assert(batteryIsOk(35, 95, 0.9) == false);
-  assert(batteryIsOk(55, 115, 1) == false);
-  assert(batteryIsOk(55, 45, 0.5) == false);
-  assert(batteryIsOk(25, 115, 0.7) == false);
-  assert(batteryIsOk(40, 75, 1) == false);
-}
+// int main() {
+//   assert(batteryIsOk(25, 70, 0.7) == true);
+//   assert(batteryIsOk(50, 85, 0) == false);
+//   assert(batteryIsOk(55, 65, 1) == false);
+//   assert(batteryIsOk(35, 95, 0.9) == false);
+//   assert(batteryIsOk(55, 115, 1) == false);
+//   assert(batteryIsOk(55, 45, 0.5) == false);
+//   assert(batteryIsOk(25, 115, 0.7) == false);
+//   assert(batteryIsOk(40, 75, 1) == false);
+// }
