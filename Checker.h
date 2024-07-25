195bed88d9df72949bd9eb80891030266f5b0167
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 15:46:55 +0530

    Update Checker.h

--- Checker.h
+++ Checker.h
@@ -3,6 +3,5 @@
 #include <string>
 #include "PrintMessage.h"
 
-extern Language PrintLanguage;
 const float CHARGE_RATE_LOWER_LIMIT = (float) NULL;
 bool batteryIsOk(float temperature, float soc, float chargeRate) ;
