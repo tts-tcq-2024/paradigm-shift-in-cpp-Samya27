2d2d55be4aad8853b1699f099814ad7d9654b659
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 12:16:52 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -1,32 +1,40 @@
 #include <assert.h>
 #include <iostream>
+#include "Message.hpp"
+#include "Checker.hpp"
 using namespace std;
 
-bool temperatureIsOk(float temperature)
+
+bool temperatureIsOk(float temperature) 
 {
-  if(temperature < 0 || temperature > 45) 
+  if (temperature < TEMPERATURE_LOWER_LIMIT || temperature > TEMPERATURE_UPPER_LIMIT)
   {
-    cout << "Temperature out of range!\n";
+    printMessage("Temperature out of range!\n", "Temperatur außerhalb des Bereichs!\n");
     return false;
   }
+  printWarningIfNeeded(temperature, TEMPERATURE_UPPER_LIMIT, TEMPERATURE_LOWER_LIMIT, "Warning: Approaching Temperature Peak", NULL);
   return true;
 }
-bool socIsOk(float soc)
+
+bool socIsOk(float soc) 
 {
-  if(soc < 20 || soc > 80) 
+  if (soc < CHARGE_RATE_LOWER_LIMIT || soc > SOC_UPPER_LIMIT) 
   {
-    cout << "State of Charge out of range!\n";
+    printMessage("State of Charge out of range!\n", "Ladezustand außerhalb des Bereichs!\n");
     return false;
   }
+  printWarningIfNeeded(soc, SOC_UPPER_LIMIT, SOC_LOWER_LIMIT, "Warning: Approaching charge-peak", "Warning: Approaching discharge");
   return true;
 }
-bool chargeRateIsOk(float chargeRate)
+
+bool chargeRateIsOk(float chargeRate) 
 {
-  if(chargeRate > 0.8) 
+  if (chargeRate > CHARGE_RATE_UPPER_LIMIT) 
   {
-    cout << "Charge Rate out of range!\n";
+    printMessage("Charge Rate out of range!\n", "Lade-Rate außerhalb des Bereichs!\n");
     return false;
   }
+  printWarningIfNeeded(chargeRate, CHARGE_RATE_UPPER_LIMIT, CHARGE_RATE_LOWER_LIMIT,  "Warning: Approaching charge rate -peak", NULL);
   return true;
 }
 
@@ -34,6 +42,7 @@ bool batteryIsOk(float temperature, float soc, float chargeRate)
 {
   return(temperatureIsOk(temperature)&socIsOk(soc)&chargeRateIsOk(chargeRate));
 }
+
 int main() {
   assert(batteryIsOk(25, 70, 0.7) == true);
   assert(batteryIsOk(50, 85, 0) == false);
