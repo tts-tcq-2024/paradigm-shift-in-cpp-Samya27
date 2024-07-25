8e9c8687194f7a7560e0bc5eb38abef392825750
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 12:50:46 +0530

    Update PrintMessage.hpp

--- PrintMessage.hpp
+++ PrintMessage.hpp
@@ -2,7 +2,6 @@
 #include <string>
 
 enum Language { ENGLISH, GERMAN };
-Language currentLanguage = ENGLISH;
 
 
 void printMessage(const std::string& messageEnglish, const std::string& messageGerman);
