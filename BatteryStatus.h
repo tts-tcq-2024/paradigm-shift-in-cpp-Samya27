4d20d057e51ddca8d219800aa687f4a7a72cf48d
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 16:32:02 +0530

    Update BatteryStatus.h

--- BatteryStatus.h
+++ BatteryStatus.h
@@ -3,5 +3,16 @@
 #include <string>
 #include "PrintMessage.h"
 
+typedef struct Battery{
+const float temperature_p[];
+const float soc_p[2];
+const float chargeRate_p[2];
+}Battery
+
+const Battery myBattery = {
+    {0,45},
+    {20,80},
+    {CHARGE_RATE_LOWER_LIMIT, 0.8}};
+
 const float CHARGE_RATE_LOWER_LIMIT = (float) NULL;
 bool batteryIsOk(float temperature, float soc, float chargeRate) ;
