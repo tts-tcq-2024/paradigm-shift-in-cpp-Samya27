32c752b7a4f9774c64ddee1cf3e5a7f8be9ac42f
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 17:00:51 +0530

    Update BatteryStatus.cpp

--- BatteryStatus.cpp
+++ BatteryStatus.cpp
@@ -5,52 +5,36 @@
 using namespace std;
 
 extern Language PrintLanguage = ENGLISH;
-bool IsInRange(float value, float UpperLimit, float Lowerlimit, const string& key)
+bool islessthan(float value, float UpperLimit, const string& key)
 {
-  //if(Lowerlimit!= (float) NULL){
-  bool indicate_flag = (Lowerlimit!= (float) NULL) && ((value<Lowerlimit) || (value>UpperLimit));
-    if(indicate_flag){
-      printMessage(PrintLanguage ,key);
-      return false;
-    }
-  //}
-  //else{
     if(value>UpperLimit){
       printMessage(PrintLanguage ,key);
       return false;
     }
-  //}
+  return true;
+}
+bool IsInRange(float value, float UpperLimit, float Lowerlimit, const string& key)
+{
+  if((value<Lowerlimit) || (value>UpperLimit)){
+    printMessage(PrintLanguage ,key);
+    return false;
+  }
   return true;
 }
 
 bool temperatureIsOk(float temperature)
 {
   bool status = IsInRange(temperature, 45, 0, "Temperature");
-  // if(temperature < 0 || temperature > 45) 
-  // {
-  //   printMessage("Temperature out of range!\n", "Temperatur außerhalb des Bereichs!\n");
-  //   return false;
-  // }
   return status;
 }
 bool socIsOk(float soc)
 {
   bool status = IsInRange(soc, 80, 20, "soc");
-  // if(soc < 20 || soc > 80) 
-  // {
-  //   printMessage("State of Charge out of range!\n", "Ladezustand außerhalb des Bereichs!\n");
-  //   return false;
-  // }
   return status;
 }
 bool chargeRateIsOk(float chargeRate)
 {
-  bool status = IsInRange(chargeRate, 0.8, CHARGE_RATE_LOWER_LIMIT, "chargeRate");
-  // if(chargeRate > 0.8) 
-  // {
-  //   printMessage("Charge Rate out of range!\n", "Lade-Rate außerhalb des Bereichs!\n");
-  //   return false;
-  // }
+  bool status = islessthan(chargeRate, 0.8, "chargeRate");
   return status;
 }
 
@@ -67,13 +51,3 @@ bool batteryIsOk(float temperature, float soc, float chargeRate)
   }
   return(BatteryStatus);
 }
-// int main() {
-//   assert(batteryIsOk(25, 70, 0.7) == true);
-//   assert(batteryIsOk(50, 85, 0) == false);
-//   assert(batteryIsOk(55, 65, 1) == false);
-//   assert(batteryIsOk(35, 95, 0.9) == false);
-//   assert(batteryIsOk(55, 115, 1) == false);
-//   assert(batteryIsOk(55, 45, 0.5) == false);
-//   assert(batteryIsOk(25, 115, 0.7) == false);
-//   assert(batteryIsOk(40, 75, 1) == false);
-// }
