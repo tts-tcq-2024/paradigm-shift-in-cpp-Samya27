9dd650f896b35b88fc224034e15846a3de2b19b2
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 13:17:48 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -45,6 +45,12 @@ bool batteryIsOk(float temperature, float soc, float chargeRate)
   return(BatteryStatus);
 }
 int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
+  assert(batteryIsOk(25, 70, 0.7) == true); // all in range
+  assert(batteryIsOk(50, 85, 0) == false); // temperature and soc out of range
+  assert(batteryIsOk(55, 65, 1) == false); // temperature and charge rate out of range
+  assert(batteryIsOk(35, 95, 0.9) == false); // soc and charge rate out of range
+  assert(batteryIsOk(55, 115, 1) == false); // all out of range
+  assert(batteryIsOk(55, 45, 0.5) == false); // temperature out of range
+  assert(batteryIsOk(25, 115, 0.7) == false); // soc out of range
+  assert(batteryIsOk(40, 75, 1) == false); // charge rate out of range
 }
