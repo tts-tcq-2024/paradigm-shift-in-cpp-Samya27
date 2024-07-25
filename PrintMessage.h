0e5516c95f4f80a08c48c4ab7ec25f1b1043520a
Samya Sharma <68061663+Samya27@users.noreply.github.com>
2024-07-25 15:12:01 +0530

    Update PrintMessage.h

--- PrintMessage.h
+++ PrintMessage.h
@@ -2,6 +2,6 @@
 #include <string>
 
 enum Language { ENGLISH, GERMAN };
-
+enum Language PrintLanguage = ENGLISH; 
 
 void printMessage(const std::string& messageEnglish, const std::string& messageGerman);
